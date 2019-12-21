e480c44cafe9506ef989a56ac336d4ee701f8796
shuttlesaifdev <saiful@shuttlebd.com>
2019-12-21 18:33:07 +0600

    get all task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,23 +12,11 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
   ) {}
-  // getAllTasks(): Task[] {
-  //     return this.tasks;
-  // }
-  // getTaskWithFilter(filterDto: GetTaskFilterDto): Task[]{
-  //     const { status ,search } = filterDto;
-  //     let tasks = this.getAllTasks();
-  //     if(status){
-  //         tasks = tasks.filter(task => task.status === status);
-  //     }
-  //     if(search){
-  //         tasks = tasks.filter(task =>
-  //             task.title.includes(search) ||
-  //             task.description.includes(search)
-  //         )
-  //     }
-  //     return tasks;
-  // }
+
+  async getTasks(filterDto: GetTaskFilterDto) {
+    return this.taskRepository.getTasks(filterDto);
+  }
+
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
     if (!found) {
